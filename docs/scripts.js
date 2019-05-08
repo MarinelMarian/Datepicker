71975c7aa6768bfacfeeb4e2a100616ffc76fc4d
Marinel Marian <marinel.marian@gmail.com>
2019-05-08 13:33:42 +0300

    Create scripts.js

---
+++ docs/scripts.js
@@ -0,0 +1,16 @@
+angular
+  .module('Demo', ['moment-picker'])
+  .controller('DemoController', ['$scope', function () {
+    var ctrl = this;
+    ctrl.minDateString = moment('2019-04-15').format('YYYY-MM-DD');
+    ctrl.maxDateString = moment('2020-08-15').format('YYYY-MM-DD');
+    
+    // disable all Sundays in the Month View
+    ctrl.isSelectable = function (date, type) {
+      //console.log(date);
+      //console.log(type);
+      var month = date.month()+1;
+      return month < 6 || month > 7 
+      //return type != 'day' || date.format('dddd') != 'Sunday';
+    };
+  }]);
